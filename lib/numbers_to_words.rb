733ae7823ab25ac636f63a413d8447f18bbf92eb
Ken Rutan and sowmya dinavahi <ken@>
2017-04-18 15:57:28 -0700

    Add a debugging script to the file and finish cases with special exceptions.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,7 +1,7 @@
 class Array
   define_method(:three_digits) do
-    single_digit_hash = {"0"=>"", "1"=>"one", "2"=>"two", "3"=>"three", "4"=>"four", "5"=>"five", "6"=>"six", "7"=>"seven", "8"=>"eight", "9"=>"nine", "10"=>"ten"}
-    teens_digit_hash = {"0"=>"ten", "1"=>"eleven", "2"=>"twelve", "3"=>"thirteen", "4"=>"fourteen", "5"=>"fifteen", "6"=>"sixteen", "7"=>"seventeen", "8"=>"eighteen", "9"=>"nineteen"}
+    single_digit_hash = {"0"=>"", "1"=>"one ", "2"=>"two ", "3"=>"three ", "4"=>"four ", "5"=>"five ", "6"=>"six ", "7"=>"seven ", "8"=>"eight ", "9"=>"nine "}
+    teens_digit_hash = {"0"=>"ten ", "1"=>"eleven ", "2"=>"twelve ", "3"=>"thirteen ", "4"=>"fourteen ", "5"=>"fifteen ", "6"=>"sixteen ", "7"=>"seventeen ", "8"=>"eighteen ", "9"=>"nineteen "}
     double_digit_hash = {"2"=>"twenty ", "3"=>"thirty ", "4"=>"forty ", "5"=>"fifty ", "6"=>"sixty ", "7"=>"seventy ", "8"=>"eighty ", "9"=>"ninty "}
     output_array = []
     input_array = self
@@ -24,7 +24,7 @@ class Array
       if third_digit == "0"
       else
         output_array.push(single_digit_hash.fetch(third_digit))
-        output_array.push(" hundred ")
+        output_array.push("hundred ")
       end
       if second_digit == "1"
         teens = teens_digit_hash.fetch(first_digit)
@@ -54,19 +54,68 @@ class Fixnum
       first_three_digits.unshift(input_array2.pop())
       first_three_digits.unshift(input_array2.pop())
       output_array2.push(first_three_digits.three_digits())
+    elsif number_of_digits2 < 3
+      if input_array2 == ["0"]
+        input_array2.pop()
+        output_array2.push("zero")
+      else
+      output_array2.push(input_array2.three_digits())
+      end
     end
+
+    number_of_digits3 = input_array2.length()
+
+    if number_of_digits3 >= 3
+      first_three_digits = []
+      first_three_digits.unshift(input_array2.pop())
+      first_three_digits.unshift(input_array2.pop())
+      first_three_digits.unshift(input_array2.pop())
+      ran_already = first_three_digits.three_digits()
+      if ran_already == ""
+      else
+        output_array2.unshift("thousand ")
+        output_array2.unshift(ran_already)
+      end
+    elsif number_of_digits3 < 3 && number_of_digits3 >= 1
+      output_array2.unshift("thousand ")
+      output_array2.unshift(input_array2.three_digits())
+    end
+
     number_of_digits3 = input_array2.length()
     if number_of_digits3 >= 3
       first_three_digits = []
       first_three_digits.unshift(input_array2.pop())
       first_three_digits.unshift(input_array2.pop())
       first_three_digits.unshift(input_array2.pop())
-      output_array2.unshift(" thousand ")
-      output_array2.unshift(first_three_digits.three_digits())
-    else
-      output_array2.unshift(" thousand ")
+      ran_already2 = first_three_digits.three_digits()
+      if ran_already2 == ""
+      else
+        output_array2.unshift("million ")
+        output_array2.unshift(ran_already2)
+      end
+    elsif number_of_digits3 < 3 && number_of_digits3 >= 1
+      output_array2.unshift("million ")
       output_array2.unshift(input_array2.three_digits())
     end
+
+    number_of_digits3 = input_array2.length()
+    if number_of_digits3 >= 3
+      first_three_digits = []
+      first_three_digits.unshift(input_array2.pop())
+      first_three_digits.unshift(input_array2.pop())
+      first_three_digits.unshift(input_array2.pop())
+      ran_already3 = first_three_digits.three_digits()
+
+      if ran_already3 == ""
+      else
+        output_array2.unshift("billion ")
+        output_array2.unshift(ran_already3)
+      end
+    elsif number_of_digits3 < 3 && number_of_digits3 >= 1
+      output_array2.unshift("billion ")
+      output_array2.unshift(input_array2.three_digits())
+    end
+
     output_array2.join()
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,6 +1,7 @@
 require('rspec')
 # require('scrabble')
 require('numbers_to_words')
+require('pry')
 #
 # describe("String#scrabble") do
 #
@@ -38,15 +39,51 @@ require('numbers_to_words')
 describe("Fixnum#numbers_to_words") do
 
   it("Convert a user's single digit number input to a text word.") do
-  expect(1.numbers_to_words()).to(eq("one"))
+  expect(1.numbers_to_words()).to(eq("one "))
   end
 
   it("Convert a user's two digit number input to a text word.") do
-  expect(31.numbers_to_words()).to(eq("thirty one"))
+  expect(31.numbers_to_words()).to(eq("thirty one "))
   end
 
   it("Convert a user's two digit number in the teens input to a text word.") do
-  expect(16.numbers_to_words()).to(eq("sixteen"))
+  expect(16.numbers_to_words()).to(eq("sixteen "))
+  end
+
+  it("Convert a user's three digit number input to a text word.") do
+  expect(121.numbers_to_words()).to(eq("one hundred twenty one "))
+  end
+
+  it("Convert a user's four digit number input to a text word.") do
+  expect(1214.numbers_to_words()).to(eq("one thousand two hundred fourteen "))
+  end
+
+  it("Convert a user's five digit number input to a text word.") do
+  expect(11214.numbers_to_words()).to(eq("eleven thousand two hundred fourteen "))
+  end
+
+  it("Convert a user's six digit number input to a text word.") do
+  expect(100000.numbers_to_words()).to(eq("one hundred thousand "))
+  end
+
+  it("Convert a user's seven digit number input to a text word.") do
+  expect(1100000.numbers_to_words()).to(eq("one million one hundred thousand "))
+  end
+
+  it("Convert a user's 1000001 input to a text word.") do
+  expect(1000001.numbers_to_words()).to(eq("one million one "))
+  end
+
+  it("Convert a user's 11000 input to a text word.") do
+  expect(110000.numbers_to_words()).to(eq("one hundred ten thousand "))
+  end
+
+  it("Convert a user's 1000001 input to a text word.") do
+  expect(1000001.numbers_to_words()).to(eq("one million one "))
+  end
+
+  it("Convert a user's 1000001 input to a text word.") do
+  expect(100000001.numbers_to_words()).to(eq("one hundred million one "))
   end
 
 end
